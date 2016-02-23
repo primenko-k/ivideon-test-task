43203a28066083bb897e71c3c5e186e67d2af037
Konstantin Primenko <primenko_k@mail.ru>
2016-02-23 14:20:29 +0300

    param: add checking for valid value

--- param.h
+++ param.h
@@ -6,14 +6,18 @@ template <typename T> class Param
 public:
     Param();
     Param(T defVal);
+    Param(T defVal, T minVal, T maxVal);
 
     T get() const { return val; }
     bool set(T val);
+    void setValidRange(T minVal, T maxVal);
 
 private:
+    bool isValid(T val) const { return (val < min) || (val > max) ? false : true; }
 
 private:
     T val;
+    T min, max;
 };
 
 #include "param_impl.h"
--- param_impl.h
+++ param_impl.h
@@ -1,17 +1,41 @@
+#include <limits>
 
 template<typename T>
 Param<T>::Param() : val()
 {
+    min = std::numeric_limits<T>::min();
+    max = std::numeric_limits<T>::max();
 }
 
 template<typename T>
 Param<T>::Param(T defVal) : val(defVal)
 {
+    min = std::numeric_limits<T>::min();
+    max = std::numeric_limits<T>::max();
+}
+
+template<typename T>
+Param<T>::Param(T defVal, T minVal, T maxVal) : min(minVal), max(maxVal)
+{
+    if (max < min)
+        max = min;
+
+    if (isValid(defVal))
+        val = defVal;
+    else
+        val = min;
 }
 
 template<typename T>
 bool Param<T>::set(T val)
 {
-    this->val = val;
-    return true;
+    if (isValid(val))
+    {
+        this->val = val;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
